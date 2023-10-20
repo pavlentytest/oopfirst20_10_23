5e01c2704a895541c26e63ffb0539b33e54c90a2
Paul <impetus@yandex.ru>
2023-10-20 17:41:24 +0300

    Add private

--- src/Car.java
+++ src/Car.java
@@ -1,6 +1,24 @@
 public class Car {
-    public String name;
-    public int maxspeed;
+    private String name;
+    private int maxspeed;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMaxspeed() {
+        return maxspeed;
+    }
+
+    public void setName(String name) {
+        // проверки
+        this.name = name;
+    }
+
+    public void setMaxspeed(int maxspeed) {
+        // проверки
+        this.maxspeed = maxspeed;
+    }
 
     public Car(String name, int m) {
         this.name = name;
--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,16 @@ public class Main {
     public static void main(String[] args) {
         // Объекты класса или экземпляры
         Car toyota = new Car(); // объект класса Car
-        toyota.name = "Toyota";
-        toyota.maxspeed = 200;
-        System.out.println(toyota.name);
-        System.out.println(toyota.maxspeed);
+        toyota.setName("Toyota");
+        toyota.setMaxspeed(200);
+
+
+        System.out.println(toyota.getName());
+        System.out.println(toyota.getMaxspeed());
 
         Car skoda = new Car("Skoda",250);
-        System.out.println(skoda.maxspeed);
-        System.out.println(skoda.name);
+        System.out.println(skoda.getName());
+        System.out.println(skoda.getMaxspeed());
 
         Car fiat = new Car();
         Car car = new Car();
